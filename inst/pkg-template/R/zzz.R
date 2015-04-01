74d77d2626215ae5e734d3a3f83cb6d7f0f4f410
Johannes Rainer <johannes.rainer@eurac.edu>
2015-04-01 10:34:08 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 0a595364ee0ef282cbcc6d5b72363b6a9115dfc9
    
        + Added argument "port" to the "fetchTablesFromEnsembl" function. + removed the "ensembldb:::" in the pkg-template's zzz.R. + Added the org from which the vignette is built.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ensembldb@101709 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/pkg-template/R/zzz.R
+++ inst/pkg-template/R/zzz.R
@@ -8,7 +8,7 @@
   path <- system.file("extdata", package=pkgname, lib.loc=libname)
   files <- dir(path)
   for(i in seq_len(length(files))){
-    db <- ensembldb:::EnsDb(system.file("extdata", files[[i]], package=pkgname,
+    db <- EnsDb(system.file("extdata", files[[i]], package=pkgname,
                   lib.loc=libname))
     objname <- sub(".sqlite$","",files[[i]])
     assign(objname, db, envir=ns)
