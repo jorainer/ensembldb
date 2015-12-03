a2dd7b996a4c3b967c24810aeeed00fc495b4075
jotsetung <johannes.rainer@gmail.com>
2015-12-03 08:55:38 +0100

    Update vignette and fix unit test bug
    + Update the vignette.
    + Fix bug in an unit test.

--- inst/unitTests/test_buildEdb.R
+++ inst/unitTests/test_buildEdb.R
@@ -2,5 +2,7 @@ test_ensDbFromGRanges <- function(){
     load(system.file("YGRanges.RData", package="ensembldb"))
     DB <- ensDbFromGRanges(Y, path=tempdir(), version=75,
                            organism="Homo_sapiens", verbose=TRUE)
+    edb <- EnsDb(DB)
+    checkEquals(unname(genome(edb)), "GRCh37")
 }
 
