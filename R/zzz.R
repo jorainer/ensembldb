862fbce2e97a19f5ff3b6520ecb7279275aeef9c
jotsetung <johannes.rainer@gmail.com>
2016-02-02 08:33:13 +0100

    Add methodology to change seqlevel style
    + Implement methods seqlevelsStyle, seqlevelsStyls<-,
      supportedSeqlevelsStyles that enable to integrate EnsDb objects with
      annotation packages not using Ensembl chromosome names.
    + The seqinfo for results returned by methods such as genes, transcripts
      etc contain now only those seqnames that are present in the returned
      GRanges object.
    + Add more examples to the vignette and help pages.
    + Add more test cases.

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,15 @@
-options( useFancyQuotes=FALSE )
+
+.onLoad <- function(libname, pkgname){
+    op <- options()
+    ## What should be returned by default if the seqnames can not be mapped based
+    ## on the style set by seqlevelsStyle<-
+    ## Options:
+    ## + NA or any other value: return this value for such cases.
+    ## + MISSING: stop and throw an error.
+    ## + ORIGINAL: return the original seqnames.
+    opts.ens <- list(useFancyQuotes=FALSE,
+                     ensembldb.seqnameNotFound="ORIGINAL")
+    options(opts.ens)
+    invisible()
+}
+
