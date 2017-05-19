e844796edaf2ff67d13b77c10ac15a751a6b0d3b
jotsetung <johannes.rainer@gmail.com>
2017-05-19 07:43:40 +0200

    Passes all tests for DBSCHEMAVERSION 2.0 (and 1.0)
    - Fixes issue #48.

---
+++ inst/scripts/checkEnsDbs.R
@@ -0,0 +1,22 @@
+#' @description Check EnsDb sqlite files found in the specified folder.
+#'
+#' @param x \code{character(1)} with the folder in which we're looking for EnsDb
+#'     objects.
+#'
+#' @author Johannes Rainer
+#' 
+#' @noRd
+#'
+#' @examples
+#' dir <- "/Users/jo/tmp/ensdb_20"
+checkEnsDbs <- function(x) {
+    edbs <- dir(x, pattern = ".sqlite$", full.names = TRUE)
+    for (i in 1:length(edbs)) {
+        message("\nChecking EnsDb: ", basename(edbs[i]))
+        edb <- EnsDb(edbs[i])
+        ensembldb:::validateEnsDb(edb)
+        ## Now check also some query calls:
+        gns <- genes(edb)
+        message(" OK")
+    }
+}
