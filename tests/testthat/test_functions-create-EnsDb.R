c7713b8073190a8be5f6c9f0a99bf97247d222b3
jorainer <johannes.rainer@gmail.com>
2023-09-21 17:20:52 +0200

    fix: avoid downloading Ensembl ftp file in unit test

--- tests/testthat/test_functions-create-EnsDb.R
+++ tests/testthat/test_functions-create-EnsDb.R
@@ -249,12 +249,4 @@ test_that(".read_lines_compressed works", {
 
     res_2 <- .read_lines_compressed(fl, n = 10)
     expect_equal(res_2[1L], "#!genome-build ASM96941v1")
-
-    fl <- paste0(
-        "ftp://ftp.ensembl.org/pub/release-102/gtf/",
-        "homo_sapiens/Homo_sapiens.GRCh38.102.gtf.gz"
-    )
-    expect_warning(res <- readLines(fl, n = 2))
-    res_2 <- .read_lines_compressed(fl, n = 2)
-    expect_equal(res_2[1L], "#!genome-build GRCh38.p13")
 })
